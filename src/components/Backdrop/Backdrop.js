a3174c595b2038c5db6d622a30a1b78408827762
Mariam Fouad <mariam.fouad94@gmail.com>
2018-10-23 22:28:19 +0300

    use diffrent valus for entering and exiting

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -3,18 +3,22 @@ import Transition from 'react-transition-group/Transition';
 
 import './Backdrop.css';
 
+const timingObject={
+    enter:200,
+    exit:800,
+}
 const backdrop = (props) => {
     //const cssClasses = ["Backdrop", props.isOpen? "Backdrop--open":"Backdrop--close"];
     return <Transition
             in={props.isOpen} 
-            timeout={500}
+            timeout={timingObject}
             mountOnEnter
             unmountOnExit>
             {state=>{
                 return (
                  <div className='Backdrop'
                  style={{
-                    transition: 'opacity .5s ease-out',
+                    transition: state==='entering'?'opacity .2s ease-out':'opacity .8s ease-out',
                     opacity:(state==='entering'||state==='exiting')?'0':'1',
                   }}>
                   </div>
